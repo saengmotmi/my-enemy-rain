f598f9092f2674f0ef78d1e42ab784be01573e53
saengmotmi <ohjtack@gmail.com>
2022-06-19 17:24:53 +0900

    refactor: 게임 설정 분리

--- src/components/game.ts
+++ src/components/game.ts
@@ -1,6 +1,7 @@
 import { Ghost } from "../actors/ghost";
 import { spawnGhosts, startGhosts } from "../actors/ghost/actions";
 import { Hero } from "../actors/hero";
+import { stageSettings } from "../config";
 
 export interface StageSetting {
   stage: number;
@@ -24,24 +25,10 @@ export class Game {
       document.querySelector("#score_value") || document.createElement("div");
     this.life =
       document.querySelector("#life_value") || document.createElement("div");
-    this.stageSettings = [
-      {
-        stage: 1,
-        ghostSpeed: 0.5,
-        ghostCount: 10,
-        ghostSpawnInterval: 2000,
-      },
-      {
-        stage: 2,
-        ghostSpeed: 0.6,
-        ghostCount: 10,
-        ghostSpawnInterval: 2000,
-      },
-    ];
+    this.stageSettings = stageSettings;
   }
 
   startStage() {
-    console.log(this.hero);
     if (this.hero) {
       // 고스트 초기화
       const ghosts = spawnGhosts(this.hero);
@@ -51,35 +38,35 @@ export class Game {
 
   endGame() {
     if (this.hero) {
-      this.ghosts.forEach((ghost) => {
-        ghost.stop();
-        ghost.ghostDom.remove();
-      });
+      this.resetStage();
       this.hero.heroDom.remove();
+
+      this.ghosts = [];
+      this.hero = null;
     }
   }
 
   resetStage() {
     // TODO: 스테이지 초기화
-    this.ghosts.forEach(({ ghostDom }) => {
-      ghostDom.remove();
+    this.ghosts.forEach((ghost) => {
+      ghost.stop();
+      ghost.ghostDom.remove();
     });
     this.ghosts = [];
   }
 
   checkAllGhostsDead() {
-    const isAllGhostsDead = this.ghosts.every(
-      (ghost) => ghost.status === "dead"
-    );
-
     const isLifeRemain = Number(this.life.innerText) > 0;
-
     if (!isLifeRemain) {
       alert("게임 오버입니다.");
       this.endGame();
       // TODO: 게임오버 모달 출력
       return;
     }
+
+    const isAllGhostsDead = this.ghosts.every(
+      (ghost) => ghost.status === "dead"
+    );
     if (isAllGhostsDead) {
       this.stageIndex++;
 
--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,18 @@ export const GHOST_WIDTH = 45;
 export const HERO_WIDTH = 35;
 export const HERO_HEIGHT = 45;
 export const HERO_SPEED = 10;
+
+export const stageSettings = [
+  {
+    stage: 1,
+    ghostSpeed: 0.5,
+    ghostCount: 10,
+    ghostSpawnInterval: 2000,
+  },
+  {
+    stage: 2,
+    ghostSpeed: 0.6,
+    ghostCount: 10,
+    ghostSpawnInterval: 2000,
+  },
+];
